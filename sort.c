967a144aa2fddfa53f6f1b6da4c11e68b823ee35
Milan Castro <milan.castro1212@gmail.com>
2024-03-14 16:53:10 -0400

    Deleted exe

--- sort.c
+++ sort.c
@@ -10,7 +10,7 @@ void *Alloc(size_t sz)
 	extraMemoryAllocated += sz;
 	size_t *ret = malloc(sizeof(size_t) + sz);
 	*ret = sz;
-	printf("Extra memory allocated, size: %ld\n", sz);
+	//printf("Extra memory allocated, size: %ld\n", sz);
 	return &ret[1];
 }
 
@@ -18,7 +18,7 @@ void DeAlloc(void *ptr)
 {
 	size_t *pSz = (size_t *)ptr - 1;
 	extraMemoryAllocated -= *pSz;
-	printf("Extra memory deallocated, size: %ld\n", *pSz);
+	//printf("Extra memory deallocated, size: %ld\n", *pSz);
 	free((size_t *)ptr - 1);
 }
 
